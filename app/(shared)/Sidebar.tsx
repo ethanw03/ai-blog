6efe70d058b9c6f00c9536145af7429cf70df91c
ethanw03 <ethanwong98@yahoo.ca>
2023-05-03 07:25:45 -0700

    complete sidebar

--- app/(shared)/Sidebar.tsx
+++ app/(shared)/Sidebar.tsx
@@ -1,6 +1,9 @@
+import Image from 'next/image';
 import React from 'react';
 import SocialLinks from './SocialLinks';
 import Subscribe from './Subscribe';
+import Ad2 from '/public/assets/ad-2.png';
+import AboutProfile from '/public/assets/about-profile.jpg';
 
 type Props = {};
 
@@ -14,16 +17,30 @@ const Sidebar = (props: Props) => {
         <SocialLinks isDark />
       </div>
       <Subscribe />
-      <div className='bg-wh-900'>advert image</div>
-      <h4 className='px-5 py-3 text-xs font-bold text-center text-wh-500'>
+      <Image
+        className='hidden w-full my-8 md:block'
+        alt='advert-2'
+        placeholder='blur'
+        src={Ad2}
+        width={500}
+        height={1000}
+      />
+      <h4 className='px-5 py-3 text-xs font-bold text-center bg-wh-900 text-wh-50'>
         About the Blog
       </h4>
-      <div className='bg-wh-900'>profile image</div>
+      <div className='flex justify-center my-3'>
+        <Image
+          alt='about-profile'
+          placeholder='blur'
+          src={AboutProfile}
+          style={{ width: '500px', height: '250px', objectFit: 'cover' }}
+        />
+      </div>
       <h4 className='px-5 py-3 font-bold text-center text-wh-500'>
-        Ethan Nicholas
+        A <strong>fully</strong> autonomous blog
       </h4>
       <p className='text-sm text-center text-wh-500'>
-        Lorem vitae ut augue auctor faucibus eget eget ut libero.
+        Believe it or not! All the posts have been created using AI!
       </p>
     </section>
   );
