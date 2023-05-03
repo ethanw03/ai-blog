8cd7f404b3da3336052a5bbd268b277bfce32533
ethanw03 <ethanwong98@yahoo.ca>
2023-05-03 05:55:04 -0700

    add trending card component

--- app/(home)/Trending.tsx
+++ app/(home)/Trending.tsx
@@ -1,7 +1,30 @@
+import Link from 'next/link';
 import React from 'react';
 
-type Props = {};
+type TrendingCardProps = {
+  className?: string;
+};
 
+const TrendingCard = ({ className }: TrendingCardProps) => {
+  return (
+    <Link
+      className={`${className} sm:mt-0 sm:h-auto relative mt-7 block w-full h-96 hover:opacity-70`}
+      // href={`${process.env.NEXT_PUBLIC_BASE_URL}/post/${post?.id}`}
+      href='/'
+    >
+      <div className='relative z-0 w-full h-full bg-wh-500'>image</div>
+      <div className='absolute top-0 left-0 w-full h-full z-1 bg-gradient-gradual' />
+      <div className='absolute bottom-0 left-0 p-3 z-2'>
+        <h4 className='inline-block px-5 py-1 font-semibold bg-accent-orange text-wh-900'>
+          category
+        </h4>
+        <div className='mt-2 text-wh-100'>post title</div>
+      </div>
+    </Link>
+  );
+};
+
+type Props = {};
 const Trending = (props: Props) => {
   return (
     <section className='pt-3 pb-10'>
@@ -14,14 +37,14 @@ const Trending = (props: Props) => {
         </p>
       </div>
       <div className='flex justify-between gap-3 my-3'>
-        <div className='basis-1/2 bg-wh-500 h-96'></div>
-        <div className='flex flex-col gap-3 basis-1/2 h-96'>
-          <div className='basis-1/2 bg-wh-500'></div>
-          <div className='flex gap-3 basis-1/2'>
-            <div className='basis-1/2 bg-wh-500'></div>
-            <div className='basis-1/2 bg-wh-500'></div>
-          </div>
-        </div>
+        <TrendingCard className='basis-1/2 bg-wh-500 h-96'></TrendingCard>
+        <TrendingCard className='flex flex-col gap-3 basis-1/2 h-96'>
+          <TrendingCard className='basis-1/2 bg-wh-500'></TrendingCard>
+          <TrendingCard className='flex gap-3 basis-1/2'>
+            <TrendingCard className='basis-1/2 bg-wh-500'></TrendingCard>
+            <TrendingCard className='basis-1/2 bg-wh-500'></TrendingCard>
+          </TrendingCard>
+        </TrendingCard>
       </div>
     </section>
   );
