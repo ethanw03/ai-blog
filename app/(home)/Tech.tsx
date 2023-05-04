ec801c7cf71c51529a72a595958296c35aab4252
ethanw03 <ethanwong98@yahoo.ca>
2023-05-04 00:30:20 -0700

    add database props

--- app/(home)/Tech.tsx
+++ app/(home)/Tech.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
 import Card from '../(shared)/Card';
+import { Post } from '@prisma/client';
+import React from 'react';
 
-type Props = {};
+type Props = {
+  techPosts: Array<Post>;
+};
 
-const Tech = (props: Props) => {
+const Tech = ({ techPosts }: Props) => {
   return (
     <section>
       <hr className='border-1 ' />
@@ -21,22 +24,26 @@ const Tech = (props: Props) => {
           className='col-span-1 row-span-3 bg-wh-500'
           imageHeight='h-96'
           isLongForm
+          post={techPosts[0]}
         />
         {/* Card Small */}
         <Card
           className='flex justify-between col-span-1 row-span-1 gap-3 mt-10 bg-wh-500 sm:mt-0'
           imageHeight='h-48'
           isSmallCard
+          post={techPosts[1]}
         />
         <Card
           className='flex justify-between col-span-1 row-span-1 gap-3 mt-10 bg-wh-500 sm:mt-0'
           imageHeight='h-48'
           isSmallCard
+          post={techPosts[2]}
         />
         <Card
           className='flex justify-between col-span-1 row-span-1 gap-3 mt-10 bg-wh-500 sm:mt-0'
           imageHeight='h-48'
           isSmallCard
+          post={techPosts[3]}
         />
       </div>
     </section>
