e121d0d3b0d882ff54c9594ccc2cebf1ff3747b7
ethanw03 <ethanwong98@yahoo.ca>
2023-05-03 23:10:43 -0700

    add get routes

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,45 @@ import Travel from './(home)/Travel';
 import Other from './(shared)/Other';
 import Subscribe from './(shared)/Subscribe';
 import Sidebar from './(shared)/Sidebar';
+import { prisma } from './api/client';
+import { Post } from '@prisma/client';
 
-export default function Home() {
+const getPosts = async () => {
+  const posts = await prisma.post.findMany();
+
+  return posts;
+};
+
+export default async function Home() {
+  const posts = await getPosts();
+  console.log('posts:', posts);
+
+  const formatPost = () => {
+    const trendingPosts: Array<Post> = [];
+    const techPosts: Array<Post> = [];
+    const travelPosts: Array<Post> = [];
+    const otherPosts: Array<Post> = [];
+
+    posts.forEach((post: Post, i: number) => {
+      if (i < 4) {
+        trendingPosts.push(post);
+      } else if (post?.category === 'tech') {
+        techPosts.push(post);
+      } else if (post?.category === 'travel') {
+        travelPosts.push(post);
+      } else if (post?.category === 'other') {
+        otherPosts.push(post);
+      }
+    });
+
+    return [trendingPosts, techPosts, travelPosts, otherPosts];
+  };
+
+  const [trendingPosts, techPosts, travelPosts, otherPosts] = formatPost();
+
   return (
     <main className='px-10 leading-7'>
-      <Trending />
+      <Trending trendingPosts={trendingPosts} />
       <div className='gap-10 mb-5 md:flex'>
         <div className='basis-3/4'>
           <Tech />
